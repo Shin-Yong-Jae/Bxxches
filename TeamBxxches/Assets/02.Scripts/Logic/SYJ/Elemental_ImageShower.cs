340bf3e4d5bdd0f9d982486ef4ee09435ca8dca1
Shin-Yong-Jae <sms3284@naver.com>
2021-07-25 11:29:00 +0900

    GirlAnim&BubbleTranformChange

---
+++ TeamBxxches/Assets/02.Scripts/Logic/SYJ/Elemental_ImageShower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Elemental_ImageShower : MonoBehaviour
+{
+    public Image image;
+    public Camera camera;
+    private Transform target;
+    // Use this for initialization
+    void Start()
+    {
+        target = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        Vector3 screenPos = camera.WorldToScreenPoint(target.position);
+        float x = screenPos.x;
+        image.transform.position = new Vector3(screenPos.x,screenPos.y);
+    }
+}
